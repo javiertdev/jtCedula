0b8e11f662fad902ecd17e54fc4de549c6189470
Luis Cortes <code@javiert.dev>
2026-02-02 21:30:08 -0300

    Se corrigen las builds

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 const license = fs.readFileSync('LICENSE', 'utf8');
 
 build({
-  entryPoints: ['./dist/cjs/index.js'],
+  entryPoints: ['./dist/index.js'],
   bundle: true,
   minify: true,
   format: 'iife',
@@ -12,5 +12,8 @@ build({
   outfile: './dist/jt.cedula.min.js',
   banner: {
     js: `/*\n${license}\n*/`
+  },
+  footer: {
+    js: 'jtCedula = jtCedula.default;'
   }
 }).catch(() => process.exit(1));
